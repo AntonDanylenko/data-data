48dcf139428c15e4e0ab7fa1c1c3caa3ff84c88d
Anton Danylenko <adanylenko@stuy.edu>
2019-04-08 19:53:46 -0400

    s

--- app.py
+++ app.py
@@ -4,13 +4,12 @@ import datetime
 
 from flask import Flask, redirect, url_for, render_template, session, request, flash, get_flashed_messages
 
-# instantiate Flask object
 app = Flask(__name__)
 app.secret_key = os.urandom(32)
 
 @app.route('/', methods=['POST','GET'])
 def index():
-    return render_template("test.html") #login page
+    return render_template("index.html")
 
 if __name__ == '__main__':
     app.debug = True
--- old/app.py
+++ old/app.py
@@ -4,12 +4,13 @@ import datetime
 
 from flask import Flask, redirect, url_for, render_template, session, request, flash, get_flashed_messages
 
+# instantiate Flask object
 app = Flask(__name__)
 app.secret_key = os.urandom(32)
 
 @app.route('/', methods=['POST','GET'])
 def index():
-    return render_template("index.html")
+    return render_template("test.html") #login page
 
 if __name__ == '__main__':
     app.debug = True
